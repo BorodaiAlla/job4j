1b15d9a9f770ec3f6ffc651320323e2cb09569b4
BorodaiAlla <boroday12@gmail.com>
2018-10-28 14:08:10 +0100

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j.calculate;
 
+/**
+ * Class Calculate Выполнение задания по изучению програмирования на языке Java.
+ * @author Alla Borodai
+ * @since 0.1
+ */
+
 public class Calculate {
+	
+	/**
+	 * Main.
+	 * @param args - args.
+	 */
 	
     public static void main(String[] args) {
 	    System.out.println("Hello World");
