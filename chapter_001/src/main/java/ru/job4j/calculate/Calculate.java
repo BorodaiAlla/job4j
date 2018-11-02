c25ffb21ec014be3e96325f1efb048f76368f876
BorodaiAlla <boroday12@gmail.com>
2018-11-02 07:44:01 +0100

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -3,6 +3,7 @@ package ru.job4j.calculate;
 /**
  * Class Calculate Выполнение задания по изучению програмирования на языке Java.
  * @author Alla Borodai
+ * @version $Id$
  * @since 0.1
  */
 
@@ -12,8 +13,17 @@ public class Calculate {
 	 * Main.
 	 * @param args - args.
 	 */
-	
+	 
     public static void main(String[] args) {
 	    System.out.println("Hello World");
+    }
+
+    /**
+     * Method echo.
+     * @param name Alla Borodai.
+     * @return Echo plus Alla Borodai.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
     }
 }
