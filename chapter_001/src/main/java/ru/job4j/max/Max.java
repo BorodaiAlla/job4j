c388c4c00c10a956df5c185616b52c5efab97357
BorodaiAlla <boroday12@gmail.com>
2019-02-11 17:28:47 +0100

    4.4. Максимум из трех чисел.

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -16,7 +16,16 @@ package  ru.job4j.max;
     public int max(int first, int second) {
         return first > second ? first : second;
     }
-    public int max()
+    /**
+     * возвратить максимальное число из "temp" и "third".
+     * @param "temp" максимальное число из "first" и "second".
+     * @param "third" третий аргумент
+     * @return максимальное число.
+     */
+    public int max(int temp, int third) {
+        int temp = this.max(first, second);
+        return temp > third ? temp : third;
+    }
     /**
      * возвратить максимальное число из "first", "second", "third".
      * @param "first" первый аргумент
